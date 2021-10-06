41c2cbb6e65099fe16ce300596c63983b72b98d6
Alonso Falconi <alonso.fl@live.com>
2021-10-06 17:08:55 -0500

    Fix Rental class

--- main.rb
+++ main.rb
@@ -1,18 +1,21 @@
 require './person'
 require './student'
 require './classroom'
+require './rental'
+require './book'
 
-student = Student.new('A1', 18)
-person = Person.new(16)
+student_1 = Student.new('Math', 18, 'Student 1')
+student_2 = Student.new('Math', 16, 'Student 2')
 
-classroom = Classroom.new('A1')
+book_1 = Book.new('Book 1', 'Author 1')
+book_2 = Book.new('Book 2', 'Author 2')
 
-classroom.add_student(student)
-classroom.add_student(person)
+rental_1 = Rental.new('2021-10-05', student_1, book_1)
+rental_2 = Rental.new('2021-10-06', student_1, book_2)
+p student_1.rentals.length
 
-p classroom.students.last.classroom.label
-p person.classroom.label
-person.classroom.label = 'A2'
-
-p person.classroom.label
-p classroom.students.last.classroom.label
+math_classroom = Classroom.new('Math')
+math_classroom.add_student(student_1)
+math_classroom.add_student(student_2)
+p math_classroom.students.first.name
+p math_classroom.students.last.name
