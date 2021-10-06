5201c68dffb3f7f6e533db2123d531fc3514d95b
Alonso Falconi <alonso.fl@live.com>
2021-10-06 17:15:06 -0500

    Fix Rubocop errors

--- main.rb
+++ main.rb
@@ -4,18 +4,20 @@ require './classroom'
 require './rental'
 require './book'
 
-student_1 = Student.new('Math', 18, 'Student 1')
-student_2 = Student.new('Math', 16, 'Student 2')
+student1 = Student.new('Math', 18, 'Student 1')
+student2 = Student.new('Math', 16, 'Student 2')
 
-book_1 = Book.new('Book 1', 'Author 1')
-book_2 = Book.new('Book 2', 'Author 2')
+book1 = Book.new('Book 1', 'Author 1')
+book2 = Book.new('Book 2', 'Author 2')
 
-rental_1 = Rental.new('2021-10-05', student_1, book_1)
-rental_2 = Rental.new('2021-10-06', student_1, book_2)
-p student_1.rentals.length
+rental1 = Rental.new('2021-10-05', student1, book1)
+rental2 = Rental.new('2021-10-06', student1, book2)
+p rental1.date
+p rental2.date
+p student1.rentals.length
 
 math_classroom = Classroom.new('Math')
-math_classroom.add_student(student_1)
-math_classroom.add_student(student_2)
+math_classroom.add_student(student1)
+math_classroom.add_student(student2)
 p math_classroom.students.first.name
 p math_classroom.students.last.name
