ea426e61215924c11651b9449c9723c12418beee
Alonso Falconi <alonso.fl@live.com>
2021-10-07 00:09:17 -0500

    Add method in Student class

--- main.rb
+++ main.rb
@@ -10,11 +10,9 @@ student2 = Student.new('Math', 16, 'Student 2')
 book1 = Book.new('Book 1', 'Author 1')
 book2 = Book.new('Book 2', 'Author 2')
 
-rental1 = Rental.new('2021-10-05', student1, book1)
-rental2 = Rental.new('2021-10-06', student1, book2)
-p rental1.date
-p rental2.date
+book1.add_rental('2021-10-07', student1)
 p student1.rentals.length
+p book1.rentals
 
 math_classroom = Classroom.new('Math')
 math_classroom.add_student(student1)
